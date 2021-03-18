9c4ea85ec79932d372c69b753c37b8341b362351
eruca <nickwill1984@126.com>
2021-03-18 11:26:40 +0800

    删除无用代码

--- types/action.go
+++ types/action.go
@@ -41,31 +41,3 @@ func HandlerFunc(tabler Tabler, pt ParamType, handlers ...Action) ContextConfig 
 		})
 	}
 }
-
-// BuildFetchHandleFunc 创建Fetch通用的函数
-// func BuildFetchHandleFunc(tabler Tabler, handlers ...Action) ContextConfig {
-// 	return func(c *Context) {
-// 		c.Tabler = tabler
-// 		params := QueryParams{}
-// 		err := json.Unmarshal(c.Request.Payload, &params)
-// 		if err != nil {
-// 			panic(err)
-// 		}
-// 		// 初始化一下，设置一些默认值
-// 		// params.Init()
-// 		// 保存params的目的是为了计算CacheKey
-// 		// 如果没有缓存需求的, params不应该有值
-// 		c.Parameters = &params
-
-// 		c.Executor.actions = make([]Action, 0, len(handlers)+1)
-// 		c.AddActions(handlers...)
-// 		c.AddActions(func(c *Context) fmt.Stringer {
-// 			pairs := tabler.Query(c.DB, params)
-// 			response := BuildFromRequest(c.Request, true)
-// 			response.Add(pairs...)
-// 			c.Responder = response
-
-// 			return bytes.NewBuffer(response.JSON())
-// 		})
-// 	}
-// }
