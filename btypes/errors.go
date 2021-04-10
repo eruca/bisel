9799e88c9fc5bda2a41be7bc1dae343b75b65aae
eruca <nickwill1984@126.com>
2021-04-10 18:06:13 +0800

    增加账号密码错误的error

--- btypes/errors.go
+++ btypes/errors.go
@@ -3,8 +3,9 @@ package btypes
 import "errors"
 
 var (
-	ErrOptimisticLock = errors.New("乐观锁错误: 数据已经被修改，请刷新后重新请求")
-	ErrGroup          = NewErrorGroup()
+	ErrOptimisticLock                      = errors.New("乐观锁错误: 数据已经被修改，请刷新后重新请求")
+	ErrAccountNotExistOrPasswordNotCorrect = errors.New("账号不存在或密码错误")
+	ErrGroup                               = NewErrorGroup()
 )
 
 type ErrorGroup []PairString
