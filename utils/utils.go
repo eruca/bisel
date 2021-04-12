3b124151824446ebaf802e09625e045bd4ede6ec
eruca <nickwill1984@126.com>
2021-04-12 22:59:25 +0800

    结构体转Map时，取gorm:"column:xxx" json:"xxx"或者name 小写加下划线

--- utils/utils.go
+++ utils/utils.go
@@ -3,6 +3,7 @@ package utils
 import (
 	"os"
 	"reflect"
+	"strings"
 )
 
 func IsExist(filename string) bool {
@@ -16,11 +17,56 @@ func IsNotExist(filename string) bool {
 
 func Struct2Map(obj interface{}) map[string]interface{} {
 	t := reflect.TypeOf(obj)
+
+	for t.Kind() == reflect.Ptr {
+		t = t.Elem()
+	}
+
 	v := reflect.ValueOf(obj)
+	v = reflect.Indirect(v)
 
 	var data = make(map[string]interface{})
+top:
 	for i := 0; i < t.NumField(); i++ {
-		data[t.Field(i).Name] = v.Field(i).Interface()
+		name, ok := t.Field(i).Tag.Lookup("gorm")
+		if ok {
+		inner:
+			for _, part := range strings.Split(name, ";") {
+				kv := strings.Split(part, ":")
+				if len(kv) != 2 {
+					continue inner
+				}
+				if kv[0] == "column" {
+					data[kv[1]] = v.Field(i).Interface()
+					continue top
+				}
+			}
+		}
+
+		name, ok = t.Field(i).Tag.Lookup("json")
+		if ok && name != "-" {
+			name = strings.TrimSuffix(name, ",omitempty")
+			data[name] = v.Field(i).Interface()
+			continue
+		}
+
+		data[camelToSnake(t.Field(i).Name)] = v.Field(i).Interface()
 	}
 	return data
+}
+
+func camelToSnake(s string) string {
+	out := make([]rune, 0, len(s)*2)
+	for i, c := range s {
+		if c >= 'A' && c <= 'Z' {
+			if i == 0 {
+				out = append(out, c-'A'+'a')
+			} else {
+				out = append(out, '_', c-'A'+'a')
+			}
+		} else {
+			out = append(out, c)
+		}
+	}
+	return string(out)
 }
