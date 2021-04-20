c09a904c0620a44a72891650af450fa3886856e7
eruca <nickwill1984@126.com>
2021-04-20 22:16:54 +0800

    简化代码，增加rebuild cache

--- middlewares/cache.go
+++ middlewares/cache.go
@@ -1,6 +1,8 @@
 package middlewares
 
 import (
+	"fmt"
+
 	"github.com/eruca/bisel/btypes"
 )
 
@@ -26,8 +28,7 @@ func UseCache(c *btypes.Context) btypes.PairStringer {
 	// 如果客户端请求没有Hash这个值或者要求强制走数据库，就是没有缓存过
 	// 直接跳过
 	if params.ReforceUpdated {
-		noExistInCache(c, params)
-		return btypes.PairStringer{Key: PairKeyCache, Value: nil}
+		return noExistInCache(c, params)
 	}
 
 	// 发送过来的请求没有Hash
@@ -40,8 +41,7 @@ func UseCache(c *btypes.Context) btypes.PairStringer {
 			c.Responder = rb
 			return btypes.PairStringer{Key: PairKeyCache, Value: btypes.ValueString(value)}
 		} else {
-			noExistInCache(c, params)
-			return btypes.PairStringer{Key: PairKeyCache, Value: nil}
+			return noExistInCache(c, params)
 		}
 	}
 
@@ -50,18 +50,17 @@ func UseCache(c *btypes.Context) btypes.PairStringer {
 	// 如果客户端请求有Hash，可是在缓存中无法找到
 	// 表示缓存已经被删除
 	if !ok {
-		noExistInCache(c, params)
-		return btypes.PairStringer{Key: "Use Cache", Value: nil}
+		return noExistInCache(c, params)
 	}
 
 	// 如果缓存有数据，就直接返回给客户端了
 	// 那么后面的actions都不执行了，如果有想要执行的action必须在缓存之前
 	//! 查询了缓存后，不需要把Hash再一次给客户端，原来的Hash值还是有效的, 也不需要给数据，因为客户端有了
 	c.Responder = btypes.BuildFromRequest(c.ConfigResponseType, c.Request, true)
-	return btypes.PairStringer{Key: "Use Cache", Value: btypes.ValueString("response without payload")}
+	return btypes.PairStringer{Key: PairKeyCache, Value: btypes.ValueString("response without payload")}
 }
 
-func noExistInCache(c *btypes.Context, params *btypes.QueryParams) {
+func noExistInCache(c *btypes.Context, params *btypes.QueryParams) btypes.PairStringer {
 	// 先进行后面的操作，返回的时候应该已经有Response了
 	// 就可以对其进行缓存
 	c.Next()
@@ -74,4 +73,9 @@ func noExistInCache(c *btypes.Context, params *btypes.QueryParams) {
 	c.Cacher.Set(c.Tabler.TableName(), key, c.Responder.JSON())
 	// 给返回的结果增加Hash值，下次请求带上这个哈希值，就可以使用缓存了
 	c.Responder.AddHash(key)
+
+	return btypes.PairStringer{
+		Key:   PairKeyCache,
+		Value: btypes.ValueString(fmt.Sprintf("rebuild from %s: %v", c.Request.Type, *params)),
+	}
 }
