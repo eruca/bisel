3491e07423fba961031136ae65cbd3097e2310a9
eruca <nickwill1984@126.com>
2021-04-24 21:41:14 +0800

    代码localize

--- middlewares/cache.go
+++ middlewares/cache.go
@@ -32,8 +32,7 @@ func UseCache(c *btypes.Context) btypes.PairStringer {
 	}
 
 	cacheKey := params.BuildCacheKey(c.Request.Type)
-	value, ok := c.Cacher.Get(cacheKey)
-	if ok {
+	if value, ok := c.Cacher.Get(cacheKey); ok {
 		c.Responder = btypes.NewRawBytes(value)
 		return btypes.PairStringer{Key: PairKeyCache, Value: btypes.ValueString(value)}
 	}
