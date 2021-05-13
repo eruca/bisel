835f870a8c21b711514e57d374057892ef225a1b
eruca <nickwill1984@126.com>
2021-05-13 23:08:16 +0800

    实现了stderr与file logger

--- btypes/logger.go
+++ btypes/logger.go
@@ -1,6 +1,7 @@
 package btypes
 
 import (
+	"fmt"
 	"io"
 	"os"
 
@@ -17,111 +18,227 @@ const (
 	LogFile
 )
 
-type Logger struct {
-	*zap.SugaredLogger
-}
+type LogLevel int
 
-func getLoggerLevel(lvl string) zapcore.Level {
+const (
+	LogDebug LogLevel = iota
+	LogInfo
+	LogWarn
+	LogError
+)
+
+func convertToLogLevel(lvl string) LogLevel {
 	switch lvl {
 	case "debug":
-		return zapcore.DebugLevel
+		return LogDebug
 	case "info":
-		return zapcore.InfoLevel
+		return LogInfo
 	case "warn":
-		return zapcore.WarnLevel
+		return LogWarn
 	case "error":
-		return zapcore.ErrorLevel
-	case "dpanic":
-		return zapcore.DPanicLevel
-	case "panic":
-		return zapcore.PanicLevel
-	case "fatal":
-		return zapcore.FatalLevel
+		return LogError
 	}
-
-	return zapcore.InfoLevel
+	panic("should not happened:'" + lvl + "'")
 }
 
-func NewLogger(filename, lvl string, logTargets LogTarget) (Logger, io.Writer) {
-	level := getLoggerLevel(lvl)
+type Logger interface {
+	Debugf(string, ...interface{})
+	Infof(string, ...interface{})
+	Warnf(string, ...interface{})
+	Errorf(string, ...interface{})
+}
 
-	var writers []io.Writer
-	if logTargets&LogStderr == LogStderr {
-		writers = append(writers, os.Stderr)
-	}
-	if logTargets&LogFile == LogFile {
-		if filename == "" {
-			panic("未设置文件名，但要求文件记录Log")
-		}
-		writers = append(writers, &lumberjack.Logger{
-			Filename:  filename,
-			MaxAge:    30,
-			MaxSize:   10,
-			LocalTime: true,
+// Colors
+const (
+	Reset       = "\033[0m"
+	Red         = "\033[31m"
+	Green       = "\033[32m"
+	Yellow      = "\033[33m"
+	Blue        = "\033[34m"
+	Magenta     = "\033[35m"
+	Cyan        = "\033[36m"
+	White       = "\033[37m"
+	BlueBold    = "\033[34;1m"
+	MagentaBold = "\033[35;1m"
+	RedBold     = "\033[31;1m"
+	YellowBold  = "\033[33;1m"
+
+	debugStr      = "[debug] %s\n"
+	debugStrColor = Blue + "[debug]" + Reset + Cyan + "%s\n" + Reset
+	infoStr       = "[info] %s\n"
+	infoStrColor  = Green + "[info] " + Reset + Green + "%s\n" + Reset
+	warnStr       = "[warn] %s\n"
+	warnStrColor  = Magenta + "[warn] " + Reset + BlueBold + "%s\n" + Reset
+	errStr        = "[error] %s\n"
+	errStrColor   = Red + "[error] " + Reset + Magenta + "%s\n" + Reset
+)
+
+func NewLogger(logTarget LogTarget, logConfig Logging) (targets MultiTargets) {
+	if logTarget&LogStderr == LogStderr {
+		targets = append(targets, &stderr{
+			target:   target{os.Stderr},
+			level:    convertToLogLevel(logConfig.StderrLevel),
+			colorful: logConfig.StderrColor,
 		})
 	}
 
-	writer := io.MultiWriter(writers...)
-	syncWriter := zapcore.AddSync(writer)
+	if logTarget&LogFile == LogFile {
+		targets = append(targets, NewFileLogger(logConfig.Filename, convertToLogLevel(logConfig.Level)))
+	}
+
+	return
+}
+
+// ********************************** MultiTargets *********************************************
+type MultiTargets []Logger
+
+func (mt MultiTargets) Debugf(tmpl string, args ...interface{}) {
+	for _, target := range mt {
+		target.Debugf(tmpl, args...)
+	}
+}
+
+func (mt MultiTargets) Infof(tmpl string, args ...interface{}) {
+	for _, target := range mt {
+		target.Infof(tmpl, args...)
+	}
+}
+
+func (mt MultiTargets) Warnf(tmpl string, args ...interface{}) {
+	for _, target := range mt {
+		target.Warnf(tmpl, args...)
+	}
+}
+
+func (mt MultiTargets) Errorf(tmpl string, args ...interface{}) {
+	for _, target := range mt {
+		target.Errorf(tmpl, args...)
+	}
+}
+
+type target struct {
+	io.Writer
+}
+
+func (t target) Debugf(tmpl string, args ...interface{}) {
+	t.Write([]byte(fmt.Sprintf(tmpl, args...)))
+}
+
+func (t target) Infof(tmpl string, args ...interface{}) {
+	t.Write([]byte(fmt.Sprintf(tmpl, args...)))
+
+}
+
+func (t target) Warnf(tmpl string, args ...interface{}) {
+	t.Write([]byte(fmt.Sprintf(tmpl, args...)))
+}
+
+func (t target) Errorf(tmpl string, args ...interface{}) {
+	t.Write([]byte(fmt.Sprintf(tmpl, args...)))
+}
+
+// ***************************** stderr logger ********************************
+type stderr struct {
+	target
+	level    LogLevel
+	colorful bool
+}
+
+func (sd *stderr) Debugf(tmpl string, args ...interface{}) {
+	if sd.level >= LogDebug {
+		text := fmt.Sprintf(tmpl, args...)
+
+		levelTmpl := debugStr
+		if sd.colorful {
+			levelTmpl = debugStrColor
+		}
+		sd.target.Debugf(levelTmpl, text)
+	}
+}
+
+func (sd *stderr) Infof(tmpl string, args ...interface{}) {
+	if sd.level >= LogInfo {
+		text := fmt.Sprintf(tmpl, args...)
+		levelTmpl := infoStr
+		if sd.colorful {
+			levelTmpl = infoStrColor
+		}
+		sd.target.Debugf(levelTmpl, text)
+	}
+}
+
+func (sd *stderr) Warnf(tmpl string, args ...interface{}) {
+	if sd.level >= LogWarn {
+		text := fmt.Sprintf(tmpl, args...)
+		levelTmpl := warnStr
+		if sd.colorful {
+			levelTmpl = warnStrColor
+		}
+		sd.target.Debugf(levelTmpl, text)
+	}
+}
+
+func (sd *stderr) Errorf(tmpl string, args ...interface{}) {
+	if sd.level >= LogError {
+		text := fmt.Sprintf(tmpl, args...)
+		levelTmpl := errStr
+		if sd.colorful {
+			levelTmpl = errStrColor
+		}
+		sd.target.Debugf(levelTmpl, text)
+	}
+}
+
+//*********************** file logger *******************************
+type ZapLogger struct {
+	*zap.SugaredLogger
+}
+
+func getLogLevel(l LogLevel) zapcore.Level {
+	switch l {
+	case LogDebug:
+		return zapcore.DebugLevel
+	case LogInfo:
+		return zapcore.InfoLevel
+	case LogWarn:
+		return zapcore.WarnLevel
+	case LogError:
+		return zapcore.ErrorLevel
+	}
+	return zapcore.InfoLevel
+}
+
+func NewFileLogger(filename string, lvl LogLevel) Logger {
+	if filename == "" {
+		panic("未设置文件名，但要求文件记录Log")
+	}
+	syncWriter := zapcore.AddSync(&lumberjack.Logger{
+		Filename:  filename,
+		MaxAge:    30,
+		MaxSize:   10,
+		LocalTime: true,
+	})
 	encoder := zap.NewProductionEncoderConfig()
 	encoder.EncodeTime = zapcore.ISO8601TimeEncoder
+
+	level := getLogLevel(lvl)
 	core := zapcore.NewCore(zapcore.NewJSONEncoder(encoder), syncWriter, zap.NewAtomicLevelAt(level))
 	log := zap.New(core, zap.AddCaller(), zap.AddCallerSkip(1))
-	return Logger{log.Sugar()}, writer
+	return &ZapLogger{SugaredLogger: log.Sugar()}
 }
 
-func (logger *Logger) Debug(args ...interface{}) {
-	logger.SugaredLogger.Debug(args...)
-}
-
-func (logger *Logger) Debugf(template string, args ...interface{}) {
+func (logger *ZapLogger) Debugf(template string, args ...interface{}) {
 	logger.SugaredLogger.Debugf(template, args...)
 }
 
-func (logger *Logger) Info(args ...interface{}) {
-	logger.SugaredLogger.Info(args...)
-}
-
-func (logger *Logger) Infof(template string, args ...interface{}) {
+func (logger *ZapLogger) Infof(template string, args ...interface{}) {
 	logger.SugaredLogger.Infof(template, args...)
 }
 
-func (logger *Logger) Warn(args ...interface{}) {
-	logger.SugaredLogger.Warn(args...)
-}
-
-func (logger *Logger) Warnf(template string, args ...interface{}) {
+func (logger *ZapLogger) Warnf(template string, args ...interface{}) {
 	logger.SugaredLogger.Warnf(template, args...)
 }
 
-func (logger *Logger) Error(args ...interface{}) {
-	logger.SugaredLogger.Error(args...)
-}
-
-func (logger *Logger) Errorf(template string, args ...interface{}) {
+func (logger *ZapLogger) Errorf(template string, args ...interface{}) {
 	logger.SugaredLogger.Errorf(template, args...)
-}
-
-func (logger *Logger) DPanic(args ...interface{}) {
-	logger.SugaredLogger.DPanic(args...)
-}
-
-func (logger *Logger) DPanicf(template string, args ...interface{}) {
-	logger.SugaredLogger.DPanicf(template, args...)
-}
-
-func (logger *Logger) Panic(args ...interface{}) {
-	logger.SugaredLogger.Panic(args...)
-}
-
-func (logger *Logger) Panicf(template string, args ...interface{}) {
-	logger.SugaredLogger.Panicf(template, args...)
-}
-
-func (logger *Logger) Fatal(args ...interface{}) {
-	logger.SugaredLogger.Fatal(args...)
-}
-
-func (logger *Logger) Fatalf(template string, args ...interface{}) {
-	logger.SugaredLogger.Fatalf(template, args...)
 }
